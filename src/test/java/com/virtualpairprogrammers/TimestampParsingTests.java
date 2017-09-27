a0402954ef7f5fbc443f35bf53261f2c6f818d64
yoogesh2002 <syoogesh@gmail.com>
2017-09-26 21:14:17 -0400

    Create TimestampParsingTests.java

---
+++ src/test/java/com/virtualpairprogrammers/TimestampParsingTests.java
@@ -0,0 +1,17 @@
+package com.virtualpairprogrammers;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TimestampParsingTests
+{
+
+@Test
+public void timestamp()
+{
+   String result = "onetwo";
+   assertEquals("onetwo", result);
+  
+}
+
+}
